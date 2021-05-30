4c199e8cceb53212c1ef0b074eac29a6d1851ab2
Vanya Arora <56959680+vanya-24@users.noreply.github.com>
2021-05-30 17:17:19 +0530

    Update views.py

--- covid_helper/views.py
+++ covid_helper/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from covid_helper.models import Contact
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def home(request):
@@ -38,3 +40,57 @@ def resources(request):
 	messages.success(request, 'Welcome to resources')
 	return render(request, 'covid_helper/resources.html')
 
+def handleSignup(request):
+	if request.method == 'POST':
+		# Get the post parameters
+		username = request.POST['username']
+		fname = request.POST['fname']
+		lname = request.POST['lname']
+		email = request.POST['email']
+		pass1 = request.POST['pass1']
+		pass2 = request.POST['pass2']
+
+
+		if len(username) > 10:
+			messages.error(request, 'Username must be under 10')
+			return redirect('/')
+
+		if not username.isalnum():
+			messages.error(request, 'Username must be alphanumeric')
+			return redirect('/')
+
+		if pass1 != pass2:
+			messages.error(request, 'Passwords do not match')
+			return redirect('/')
+		
+
+		myuser = User.objects.create_user(username,email,pass1)
+		myuser.fname = fname
+		myuser.lname = lname
+		myuser.save()
+		messages.success(request, 'Account registered successfully')
+		return redirect('/')
+	else:
+		return HttpResponse('404-Not Found');
+
+
+def handleLogin(request):
+	if request.method == 'POST':
+		loginusername = request.POST['loginusername']
+		loginpassword = request.POST['loginpassword']
+
+		user = authenticate(username=loginusername, password=loginpassword)
+
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Successfully Logged in")
+			return redirect('/')
+		else:
+			messages.error(request, "Invalid Credentials,Please try again")
+			return redirect('/')
+	return HttpResponse('404 - Not Found')
+
+def handlelogout(request):
+	logout(request)
+	messages.success(request,"Successfully Logged Out")
+	return redirect('/')
