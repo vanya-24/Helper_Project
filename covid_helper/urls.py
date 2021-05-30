81f52b5d07e3d012ab051106a8b9e9cc5898e729
Vanya Arora <56959680+vanya-24@users.noreply.github.com>
2021-05-30 17:16:25 +0530

    Update urls.py

--- covid_helper/urls.py
+++ covid_helper/urls.py
@@ -8,5 +8,10 @@ path('abts/',views.about,name='covid-about'),
 path('registration/',views.registration,name='covid-reg'),
 path('appointment/',views.appointment,name='covid-apt'),
 path('doctors/',views.doctors,name='covid-doc'),
-path('resources',views.resources,name='resources')
+path('resources',views.resources,name='resources'),
+path('signup',views.handleSignup,name='handleSignup'),
+path('login',views.handleLogin,name='handleLogin'),
+path('logout',views.handlelogout,name='handlelogout'),
+
 ]
+
